9324cfbf2542bf748126ae9cd945622384d7e6a9
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-05 12:32:09 +0900

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :channel, only: [:index] do
+      resources :channels do
         resources :messages, only: [:index]
       end
     end

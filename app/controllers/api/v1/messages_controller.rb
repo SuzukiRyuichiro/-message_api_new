9af9e5c7fc0ff4252c153ff02102bc56c60323fb
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-05 14:10:09 +0900

    index now works. also with channel can be chosen

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::MessagesController < ApplicationController
   def index
-    @messages = Message.all
-    render json: { status: 'SUCCESS', message: 'Loaded posts', data: posts }
+    @channel = Channel.find_by(name: params[:channel_id])
+    if @channel
+      @messages = @channel.messages
+      render json: { channel: @channel, messages: @messages}
+    else
+      render json: { channel: "not found", messages: []}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :channels do
-        resources :messages, only: [:index]
+        resources :messages, only: [:index, :create]
       end
     end
   end

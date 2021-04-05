5e4be46531fe1306dd1efb6af37b0d56fb26ac23
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-05 14:20:21 +0900

    its still a manual input but POST also made its way

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -8,4 +8,19 @@ class Api::V1::MessagesController < ApplicationController
       render json: { channel: "not found", messages: []}
     end
   end
+
+  def create
+    message = Message.new(content: params[:content], author: params[:author], channel: Channel.find_by(name: params[:channel_id]))
+    if message.save
+      render json: { status: 'SUCCESS', data: message }
+    else
+      render json: { status: 'ERROR', errors: message.errors, params: params }
+    end
+  end
+
+  private
+
+  def message_params
+    params.require.permit(:content, :author)
+  end
 end

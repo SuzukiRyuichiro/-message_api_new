e8e478bd2f3c0e896487605bca92866e9ac7f08c
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-09 14:53:43 +0900

    create routes for channels

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::ChannelsController < ApplicationController
     @channels = Channel.all
     render json: {channels: @channels}
   end
+
+  def create
+    @channel = Channel.new(params)
+  end
 end
